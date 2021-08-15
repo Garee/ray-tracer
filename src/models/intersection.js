20142a0da65a79e05beb0c2d40274ac99f861356
Gary Blackwood <gary@garyblackwood.co.uk>
2021-08-15 16:38:48 +0100

    docs: add jsdoc for Intersection

--- src/models/intersection.js
+++ src/models/intersection.js
@@ -1,4 +1,15 @@
+/**
+ * A representation of a ray intersection with an object.
+ */
 export class Intersection {
+  /**
+   * Create a new Intersection.
+   *
+   * @param {number} t - The unit of time at which the intersection occurs.
+   * @param {Shape} object - The object that the ray intersects.
+   * @param {number} u - The u value.
+   * @param {number} v - The v value.
+   */
   constructor(t, object, u, v) {
     this.t = t;
     this.u = u;
@@ -6,6 +17,16 @@ export class Intersection {
     this.object = object;
   }
 
+  /**
+   * Create a new Intersection from an object.
+   *
+   * @param {object} object - The object to create from.
+   * @param {number} object.t - The unit of time at which the intersection occurs.
+   * @param {Shape} object.object - The object that the ray intersects.
+   * @param {number} object.u - The u value.
+   * @param {number} object.v - The v value.
+   * @returns {Intersection} An intersection created from the object.
+   */
   static of({ t, u, v, object } = {}) {
     return new Intersection(t, object, u, v);
   }
